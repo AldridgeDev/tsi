8fc501280326d1f19f27217cee47e90969ad8770
Antonio Aldridge <aldridge.developer@gmail.com>
2019-02-20 14:46:29 -0500

    default favicon - make sure it's 1:1 ratio

---
+++ request-service-form.js
@@ -0,0 +1,609 @@
+{
+  "0": {
+    "title": "Request a Service",
+    "description": "",
+    "labelPlacement": "top_label",
+    "descriptionPlacement": "below",
+    "button": {
+      "type": "text",
+      "text": "Submit",
+      "imageUrl": ""
+    },
+    "fields": [
+      {
+        "type": "name",
+        "typeLabel": "Name",
+        "id": 1,
+        "errorMessage": "",
+        "fieldname": "name_1",
+        "formId": -1,
+        "label": "Name",
+        "adminLabel": "",
+        "isRequired": true,
+        "size": "medium",
+        "placeholder": "",
+        "descriptionPlacement": null,
+        "inputMask": "",
+        "inputMaskViewType": "",
+        "inputs": [
+          {
+            "id": "1.2",
+            "label": "Prefix",
+            "name": "",
+            "choices": [
+              {
+                "text": "Select",
+                "value": "",
+                "isSelected": false,
+                "price": ""
+              },
+              {
+                "text": "Mr.",
+                "value": "Mr.",
+                "isSelected": false,
+                "price": ""
+              },
+              {
+                "text": "Mrs.",
+                "value": "Mrs.",
+                "isSelected": false,
+                "price": ""
+              },
+              {
+                "text": "Miss",
+                "value": "Miss",
+                "isSelected": false,
+                "price": ""
+              },
+              {
+                "text": "Ms.",
+                "value": "Ms.",
+                "isSelected": false,
+                "price": ""
+              },
+              {
+                "text": "Dr.",
+                "value": "Dr.",
+                "isSelected": false,
+                "price": ""
+              },
+              {
+                "text": "Prof.",
+                "value": "Prof.",
+                "isSelected": false,
+                "price": ""
+              },
+              {
+                "text": "Rev.",
+                "value": "Rev.",
+                "isSelected": false,
+                "price": ""
+              }
+            ],
+            "isHidden": true,
+            "inputType": "radio"
+          },
+          {
+            "id": "1.3",
+            "label": "First",
+            "name": ""
+          },
+          {
+            "id": "1.4",
+            "label": "Middle",
+            "name": "",
+            "isHidden": true
+          },
+          {
+            "id": "1.6",
+            "label": "Last",
+            "name": ""
+          },
+          {
+            "id": "1.8",
+            "label": "Suffix",
+            "name": "",
+            "isHidden": true
+          }
+        ],
+        "labelPlacement": "",
+        "group": "advanced",
+        "showTabs": false,
+        "subLabelPlacement": "",
+        "multipleFiles": false,
+        "maxFiles": "",
+        "calculationFormula": "",
+        "calculationRounding": "",
+        "enableCalculation": "",
+        "disableQuantity": false,
+        "displayAllCategories": false,
+        "allowsPrepopulate": false,
+        "description": "",
+        "pageNumber": 1,
+        "inputType": "",
+        "cssClass": "",
+        "inputName": "",
+        "adminOnly": false,
+        "noDuplicates": false,
+        "defaultValue": "",
+        "choices": "",
+        "conditionalLogic": "",
+        "actionType": "show",
+        "logicType": "all",
+        "nameFormat": "advanced",
+        "validation": [
+          {
+            "rule": "phone",
+            "value": "false"
+          },
+          {
+            "rule": "name",
+            "value": "false"
+          },
+          {
+            "rule": "address",
+            "value": "false"
+          },
+          {
+            "rule": "email",
+            "value": "false"
+          }
+        ],
+        "rules": {
+          "phone": false,
+          "name": false,
+          "address": false,
+          "email": false
+        }
+      },
+      {
+        "type": "phone",
+        "typeLabel": "Phone",
+        "phoneFormat": "standard",
+        "id": 2,
+        "errorMessage": "",
+        "fieldname": "phone_2",
+        "formId": -1,
+        "label": "Phone",
+        "adminLabel": "",
+        "isRequired": true,
+        "size": "medium",
+        "placeholder": "",
+        "descriptionPlacement": null,
+        "inputMask": "",
+        "inputMaskViewType": "",
+        "inputs": null,
+        "labelPlacement": "",
+        "group": "advanced",
+        "showTabs": false,
+        "subLabelPlacement": "",
+        "multipleFiles": false,
+        "maxFiles": "",
+        "calculationFormula": "",
+        "calculationRounding": "",
+        "enableCalculation": "",
+        "disableQuantity": false,
+        "displayAllCategories": false,
+        "allowsPrepopulate": false,
+        "description": "",
+        "pageNumber": 1,
+        "inputType": "",
+        "cssClass": "",
+        "inputName": "",
+        "adminOnly": false,
+        "noDuplicates": false,
+        "defaultValue": "",
+        "choices": "",
+        "conditionalLogic": "",
+        "actionType": "show",
+        "logicType": "all",
+        "validation": [
+          {
+            "rule": "phone",
+            "value": "false"
+          },
+          {
+            "rule": "name",
+            "value": "false"
+          },
+          {
+            "rule": "address",
+            "value": "false"
+          },
+          {
+            "rule": "email",
+            "value": "false"
+          }
+        ],
+        "rules": {
+          "phone": false,
+          "name": false,
+          "address": false,
+          "email": false
+        }
+      },
+      {
+        "type": "email",
+        "typeLabel": "Email",
+        "id": 3,
+        "errorMessage": "",
+        "fieldname": "email_3",
+        "formId": -1,
+        "label": "Email",
+        "adminLabel": "",
+        "isRequired": true,
+        "size": "medium",
+        "placeholder": "",
+        "descriptionPlacement": null,
+        "inputMask": "",
+        "inputMaskViewType": "",
+        "inputs": null,
+        "labelPlacement": "",
+        "group": "advanced",
+        "showTabs": false,
+        "subLabelPlacement": "",
+        "multipleFiles": false,
+        "maxFiles": "",
+        "calculationFormula": "",
+        "calculationRounding": "",
+        "enableCalculation": "",
+        "disableQuantity": false,
+        "displayAllCategories": false,
+        "allowsPrepopulate": false,
+        "description": "",
+        "pageNumber": 1,
+        "inputType": "",
+        "cssClass": "",
+        "inputName": "",
+        "adminOnly": false,
+        "noDuplicates": false,
+        "defaultValue": "",
+        "choices": "",
+        "conditionalLogic": "",
+        "actionType": "show",
+        "logicType": "all",
+        "validation": [
+          {
+            "rule": "phone",
+            "value": "false"
+          },
+          {
+            "rule": "name",
+            "value": "false"
+          },
+          {
+            "rule": "address",
+            "value": "false"
+          },
+          {
+            "rule": "email",
+            "value": "false"
+          }
+        ],
+        "rules": {
+          "phone": false,
+          "name": false,
+          "address": false,
+          "email": false
+        }
+      },
+      {
+        "type": "address",
+        "typeLabel": "Address",
+        "addressType": "international",
+        "id": 4,
+        "errorMessage": "",
+        "fieldname": "address_4",
+        "formId": -1,
+        "label": "Address",
+        "adminLabel": "",
+        "isRequired": true,
+        "size": "medium",
+        "placeholder": "",
+        "descriptionPlacement": null,
+        "inputMask": "",
+        "inputMaskViewType": "",
+        "inputs": [
+          {
+            "id": "4.1",
+            "label": "Street Address",
+            "name": ""
+          },
+          {
+            "id": "4.2",
+            "label": "Address Line 2",
+            "name": ""
+          },
+          {
+            "id": "4.3",
+            "label": "City",
+            "name": ""
+          },
+          {
+            "id": "4.4",
+            "label": "State / Province",
+            "name": ""
+          },
+          {
+            "id": "4.5",
+            "label": "ZIP / Postal Code",
+            "name": ""
+          },
+          {
+            "id": "4.6",
+            "label": "Country",
+            "name": ""
+          }
+        ],
+        "labelPlacement": "",
+        "group": "advanced",
+        "showTabs": false,
+        "subLabelPlacement": "",
+        "multipleFiles": false,
+        "maxFiles": "",
+        "calculationFormula": "",
+        "calculationRounding": "",
+        "enableCalculation": "",
+        "disableQuantity": false,
+        "displayAllCategories": false,
+        "allowsPrepopulate": false,
+        "description": "",
+        "pageNumber": 1,
+        "inputType": "",
+        "cssClass": "",
+        "inputName": "",
+        "adminOnly": false,
+        "noDuplicates": false,
+        "defaultValue": "",
+        "choices": "",
+        "conditionalLogic": "",
+        "actionType": "show",
+        "logicType": "all",
+        "validation": [
+          {
+            "rule": "phone",
+            "value": "false"
+          },
+          {
+            "rule": "name",
+            "value": "false"
+          },
+          {
+            "rule": "address",
+            "value": "false"
+          },
+          {
+            "rule": "email",
+            "value": "false"
+          }
+        ],
+        "rules": {
+          "phone": false,
+          "name": false,
+          "address": false,
+          "email": false
+        }
+      },
+      {
+        "type": "select",
+        "typeLabel": "Drop Down",
+        "id": 5,
+        "errorMessage": "",
+        "fieldname": "select_5",
+        "formId": -1,
+        "label": "Time of Day That Works Best for You",
+        "adminLabel": "",
+        "isRequired": true,
+        "size": "medium",
+        "placeholder": "",
+        "descriptionPlacement": null,
+        "inputMask": "",
+        "inputMaskViewType": "",
+        "inputs": null,
+        "labelPlacement": "",
+        "group": "standard",
+        "showTabs": false,
+        "subLabelPlacement": "",
+        "multipleFiles": false,
+        "maxFiles": "",
+        "calculationFormula": "",
+        "calculationRounding": "",
+        "enableCalculation": "",
+        "disableQuantity": false,
+        "displayAllCategories": false,
+        "allowsPrepopulate": false,
+        "description": "",
+        "pageNumber": 1,
+        "inputType": "",
+        "cssClass": "",
+        "inputName": "",
+        "adminOnly": false,
+        "noDuplicates": false,
+        "defaultValue": "",
+        "choices": [
+          {
+            "value": "First Choice",
+            "text": "Early Mornings",
+            "isSelected": false,
+            "price": ""
+          },
+          {
+            "value": "Second Choice",
+            "text": "Afternoons",
+            "isSelected": false,
+            "price": ""
+          },
+          {
+            "value": "Third Choice",
+            "text": "Late Afternoons",
+            "isSelected": false,
+            "price": ""
+          }
+        ],
+        "conditionalLogic": "",
+        "actionType": "show",
+        "logicType": "all",
+        "validation": [
+          {
+            "rule": "phone",
+            "value": "false"
+          },
+          {
+            "rule": "name",
+            "value": "false"
+          },
+          {
+            "rule": "address",
+            "value": "false"
+          },
+          {
+            "rule": "email",
+            "value": "false"
+          }
+        ],
+        "rules": {
+          "phone": false,
+          "name": false,
+          "address": false,
+          "email": false
+        }
+      },
+      {
+        "type": "textarea",
+        "typeLabel": "Paragraph Text",
+        "id": 6,
+        "errorMessage": "",
+        "fieldname": "textarea_6",
+        "formId": -1,
+        "label": "Message",
+        "adminLabel": "",
+        "isRequired": false,
+        "size": "medium",
+        "placeholder": "",
+        "descriptionPlacement": null,
+        "inputMask": "",
+        "inputMaskViewType": "",
+        "inputs": null,
+        "labelPlacement": "",
+        "group": "standard",
+        "showTabs": false,
+        "subLabelPlacement": "",
+        "multipleFiles": false,
+        "maxFiles": "",
+        "calculationFormula": "",
+        "calculationRounding": "",
+        "enableCalculation": "",
+        "disableQuantity": false,
+        "displayAllCategories": false,
+        "allowsPrepopulate": false,
+        "description": "",
+        "pageNumber": 1,
+        "inputType": "",
+        "cssClass": "",
+        "inputName": "",
+        "adminOnly": false,
+        "noDuplicates": false,
+        "defaultValue": "",
+        "choices": "",
+        "conditionalLogic": "",
+        "actionType": "show",
+        "logicType": "all",
+        "validation": [
+          {
+            "rule": "phone",
+            "value": "false"
+          },
+          {
+            "rule": "name",
+            "value": "false"
+          },
+          {
+            "rule": "address",
+            "value": "false"
+          },
+          {
+            "rule": "email",
+            "value": "false"
+          }
+        ],
+        "rules": {
+          "phone": false,
+          "name": false,
+          "address": false,
+          "email": false
+        }
+      },
+      {
+        "type": "ayah",
+        "id": 7,
+        "label": "Security Check: Please solve the simple math question to continue",
+        "group": "advanced",
+        "adminLabel": "",
+        "isRequired": true,
+        "size": "medium",
+        "errorMessage": "",
+        "inputs": null,
+        "formId": -1,
+        "pageNumber": 1,
+        "description": "",
+        "allowsPrepopulate": false,
+        "inputMask": false,
+        "inputMaskValue": "",
+        "inputType": "",
+        "labelPlacement": "",
+        "descriptionPlacement": "",
+        "subLabelPlacement": "",
+        "placeholder": "",
+        "cssClass": "",
+        "inputName": "",
+        "adminOnly": false,
+        "noDuplicates": false,
+        "defaultValue": "",
+        "choices": "",
+        "conditionalLogic": "",
+        "maxRows": 0,
+        "displayOnly": "",
+        "fieldname": "ayah_7",
+        "showTabs": false
+      }
+    ],
+    "version": "1.9.15.16",
+    "id": -1,
+    "useCurrentUserAsAuthor": true,
+    "postContentTemplateEnabled": false,
+    "postTitleTemplateEnabled": false,
+    "postTitleTemplate": "",
+    "postContentTemplate": "",
+    "lastPageButton": null,
+    "pagination": null,
+    "firstPageCssClass": null,
+    "replaceChoiceList": false,
+    "isModified": false,
+    "confirmations": [
+      {
+        "id": "4400c",
+        "name": "Default Confirmation",
+        "isDefault": true,
+        "type": "message",
+        "message": "Thanks for contacting us! We will get in touch with you shortly.",
+        "url": "",
+        "pageId": "",
+        "queryString": ""
+      }
+    ],
+    "notifications": [
+      {
+        "id": "bc731",
+        "to": "delawarevalleyhvac@gmail.com",
+        "name": "Admin Notification",
+        "event": "form_submission",
+        "toType": "email",
+        "subject": "New submission from {form_title}",
+        "message": "{all_fields}",
+        "fromName": "{admin_email}",
+        "replyTo": "{Email:3}"
+      }
+    ],
+    "blog_id": "28361"
+  }
+}
