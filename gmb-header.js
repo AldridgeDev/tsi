4cf4c92e9915db4f63027299ab62207ad760dd97
Antonio Aldridge <aldridge.developer@gmail.com>
2019-02-25 15:27:49 -0500

    Adding basic accordion code

--- gmb-header.js
+++ gmb-header.js
@@ -1,2 +1,2 @@
 jQuery(document).ready(function(){
-jQuery("#access").prepend("<div id='cta' style='text-align:right !important;'><a href=' target='_blank'><img src='' width='300'></a></div>");});
+jQuery("#access").prepend("<div id='cta' style='text-align:right !important;'><a href=' target='_blank'><img src='' width='250'></a></div>");});
