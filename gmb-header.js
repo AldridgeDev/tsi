b274cc4e69abbec6a90515ad152c4cf1daf744d6
Antonio Aldridge <aldridge.developer@gmail.com>
2019-02-28 08:52:36 -0500

    Added more useful code

--- gmb-header.js
+++ gmb-header.js
@@ -1,2 +1,7 @@
+// GMB
 jQuery(document).ready(function(){
 jQuery("#access").prepend("<div id='cta' style='text-align:right !important;'><a href=' target='_blank'><img src='' width='250'></a></div>");});
+
+// text
+jQuery(document).ready(function(){
+jQuery("#access").prepend("<div id='cta' style='text-align:right !important;'><span style='font-size:2rem;color:#fff;'></span></div>");});
